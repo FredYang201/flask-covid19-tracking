5c60ffaa5452ec00be632b7b2b1bc322812cb110
FredYang201 <fredyang1402143595@gmail.com>
2020-08-31 03:40:01 -0700

    adding database

--- settings.py
+++ settings.py
@@ -9,4 +9,4 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     ENV = 'production'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'postgres://rpgwzdkugnuewy:59747d223eb59b4c7c339919c00fbce21bdfae9f7dd2c9d7ea2fac523a51c774@ec2-54-144-177-189.compute-1.amazonaws.com:5432/d4od1eo3ga2i2c'
